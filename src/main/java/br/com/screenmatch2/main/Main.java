83f45bf145802a54674c4fdb198ef5fa4f2bc353
Jonathas Batista <jonathasgo2459@gmail.com>
2024-06-22 00:11:32 -0300

    commit

--- src/main/java/br/com/screenmatch2/main/Main.java
+++ src/main/java/br/com/screenmatch2/main/Main.java
@@ -3,12 +3,12 @@ package br.com.screenmatch2.main;
 import br.com.screenmatch2.model.DadosEpisodio;
 import br.com.screenmatch2.model.DadosSerie;
 import br.com.screenmatch2.model.DadosTemporada;
+import br.com.screenmatch2.model.Episodio;
 import br.com.screenmatch2.service.ConsumoAPI;
 import br.com.screenmatch2.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     private Scanner scanner = new Scanner(System.in);
@@ -32,5 +32,54 @@ public class Main {
         temporadas.forEach(System.out::println);
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\n===== Top 10 episódios =====\n");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .peek(e -> System.out.println("Primeiro filtro(N/A) "+ e))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .peek(e -> System.out.println("Ordenação "+ e))
+                .limit(10)
+                .peek(e -> System.out.println("Limite "+ e))
+                .map(e -> e.titulo().toUpperCase())
+                .peek(e -> System.out.println("Mapeamento "+ e))
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numeroTemporada(), d))
+                )        .collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("\nDigite um trecho do título do episódio: ");
+        var trechoTitulo = scanner.nextLine();
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                .findFirst();
+        if(episodioBuscado.isPresent()){
+            System.out.println("Episódio encontrado!");
+            System.out.println("Temporada: "+ episodioBuscado.get().getTemporada());
+        } else {
+            System.out.println("Episódio não encontrado!");
+        }
+
+        Map<Integer, Double> avaliacaoPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avaliacaoPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
     }
 }
---
+++ src/main/java/br/com/screenmatch2/model/Episodio.java
@@ -0,0 +1,80 @@
+package br.com.screenmatch2.model;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.List;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numeroEpisodio;
+    private LocalDate anoDeLancamento;
+    private Double avaliacao;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numeroEpisodio = dadosEpisodio.numeroEpisodio();
+
+        try {
+            this.anoDeLancamento = LocalDate.parse(dadosEpisodio.anoDeLancamento());
+        } catch (DateTimeException ex){
+            this.anoDeLancamento = null;
+        }
+
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        } catch (NumberFormatException ex){
+            this.avaliacao = 0.0;
+        }
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public LocalDate getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(LocalDate anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    @Override
+    public String toString() {
+        return "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numeroEpisodio=" + numeroEpisodio +
+                ", anoDeLancamento=" + anoDeLancamento +
+                ", avaliacao=" + avaliacao;
+    }
+}
